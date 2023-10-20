20a1e82a116544a596074ab15adf40a14f59ba24
Jesus Bermudez <jesmanuelber@gmail.com>
2023-10-20 14:27:13 -0500

    k

--- src/chart/components/DolarChart.jsx
+++ src/chart/components/DolarChart.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { ColorType, createChart } from 'lightweight-charts';
+import { BsArrowDownShort, BsArrowUpShort } from "react-icons/bs";
 import io from "socket.io-client";
 import axios from 'axios';
 
@@ -8,8 +9,11 @@ const socket = io("https://api-node-motor-finaciero-production.up.railway.app/")
 export default function DolarChart() {
     const chartContainerRef = useRef()
     const chart = useRef()
-    const [hasAppliedTimeScaleOptions, setHasAppliedTimeScaleOptions] = useState(false);
-    const [data, setData] = useState([]);
+    const [data, setData] = useState([])
+    const [barSpacing, setBarSpacing] = useState(80)
+    const [goingUp, setGoingUp] = useState(false)
+    const [changeValue, setChange] = useState(0.0)
+    const [changePercentageValue, setChangePercentage] = useState(0.0)
 
     useEffect(() => {
         if (data.length == 0) {
@@ -77,26 +81,64 @@ export default function DolarChart() {
                 width: 600,
                 height: 300
             })
+
+            // Encuentra el valor del día anterior
+            let yesterday = Math.floor((new Date(Date.now() - 24 * 60 * 60 * 1000).setUTCHours(0, 0, 0, 0)) / 1000);
+
+            let yesterdayValue = data.find(d => d.time === yesterday).value
+
+            let change = data[data.length - 1].value - yesterdayValue;
+            setChange(change)
+            setChangePercentage((change / yesterdayValue) * 100);
+
+            setGoingUp(data[data.length - 1].value >= yesterdayValue)
+
+            let lineColor = data[data.length - 1].value >= yesterdayValue ? '#34a853' : '#ea4335';
+
             const newSeries = chart.current.addAreaSeries({
-                lineColor: "#2962ff",
-                topColor: "#2962ff",
-                bottomColor: "rgba(41, 98, 255, 0.0)"
+                lineColor: lineColor,
+                topColor: lineColor,
+                bottomColor: "rgba(255, 255, 255, 0)"
             })
             newSeries.setData(data)
-            if (!hasAppliedTimeScaleOptions) {
-                chart.current.timeScale().applyOptions({
-                    barSpacing: 80,
-                });
-                setHasAppliedTimeScaleOptions(true);
-            }
+
+            chart.current.timeScale().subscribeVisibleTimeRangeChange(() => {
+                const visibleLogicalRange = chart.current.timeScale().getVisibleLogicalRange();
+                if (visibleLogicalRange) {
+                    const newBarSpacing = chart.current.timeScale().width() / (visibleLogicalRange.to - visibleLogicalRange.from);
+                    setBarSpacing(newBarSpacing);
+                }
+            });
+
+            chart.current.timeScale().applyOptions({
+                barSpacing: barSpacing,
+            });
+
 
             return () => [chart.current.remove()]
         }
     }, [data])
 
+
     return <article>
-        <h1>Dólar estadounidense a Peso colombiano</h1>
-        <p>{data.length == 0 ? "Loading..." : data[data.length - 1].value}</p>
+        <h2>Dólar estadounidense a Peso colombiano</h2>
+        <div className='article-flex'>
+            <h3>{data.length == 0 ? "Loading..." : data[data.length - 1].value}</h3>
+            <div className='article-flex__box'
+                style={{
+                    color: goingUp ? "#137333" : "#a50e0e",
+                    background: goingUp ? "rgba(47, 205, 47, 0.17)" : "rgba(205, 47, 47, 0.17)"
+                }}>
+                {goingUp ? <BsArrowUpShort className='box__icon' /> : <BsArrowDownShort className='box__icon' />}
+                <p>{Math.abs(changePercentageValue).toFixed(2)} %</p>
+            </div>
+        </div>
+        <p>{getTimeToday()} GMT-0500 (hora estándar de Colombia)</p>
         <div className='chart-container' ref={chartContainerRef}></div>
     </article>
+}
+
+function getTimeToday() {
+    let date = new Date(Date.now())
+    return `${date.getDate()} ${date.toLocaleString('default', { month: 'short' })}, ${date.getFullYear()} ${date.toLocaleTimeString()}`
 }
