0570258f601724ec16091398fecfbd9eec1751f2
Jesus Bermudez <jesmanuelber@gmail.com>
2023-11-11 21:21:27 -0500

    faltante: acciones

--- src/components/charts/PibConstanteChart.jsx
+++ src/components/charts/PibConstanteChart.jsx
@@ -2,14 +2,36 @@ import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
 import { ColorType, createChart } from "lightweight-charts";
 import { BsArrowDownShort, BsArrowUpShort } from "react-icons/bs";
+import { FaSpinner } from "react-icons/fa6";
 
 export default function PibConstanteChart() {
   const chartContainerRef = useRef();
   const chart = useRef();
+  const predictionSeries = useRef();
   const [data, setData] = useState([]);
   const [goingUp, setGoingUp] = useState(false);
   const [changeValue, setChange] = useState(0.0);
   const [changePercentageValue, setChangePercentage] = useState(0.0);
+  const [years, setYears] = useState("");
+  const [lineAdded, setLineAdded] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [toolTip, setToolTip] = useState({
+    width: "105px",
+    height: "95px",
+    position: "absolute",
+    display: "none",
+    left: "12px",
+    top: "12px",
+    padding: "8px",
+    boxSizing: "border-box",
+    fontSize: "12px",
+    textAlign: "left",
+    zIndex: "1000",
+    background: "white",
+    color: "black",
+    border: "2px solid #2962FF",
+    content: "",
+  });
 
   useEffect(() => {
     axios
@@ -70,6 +92,10 @@ export default function PibConstanteChart() {
           ? "#34a853"
           : "#ea4335";
 
+      predictionSeries.current = chart.current.addLineSeries({
+        color: "rgb(4, 111, 232)",
+      });
+
       const newSeries = chart.current.addAreaSeries({
         lineColor: lineColor,
         topColor: lineColor,
@@ -81,6 +107,104 @@ export default function PibConstanteChart() {
       return () => [chart.current.remove()];
     }
   }, [data]);
+
+  const handleButtonClick = (e) => {
+    e.preventDefault();
+    const currentYear = new Date(Date.now()).getFullYear();
+
+    if (!lineAdded) {
+      setLoading(
+        <span style={{ color: "white" }}>
+          <FaSpinner />
+        </span>
+      );
+      axios
+        .get(
+          `https://mpf.fly.dev/predicciones/macroeconomicas/pib_constante/${currentYear}/${
+            currentYear + years
+          }`
+        )
+        .then((response) => {
+          const container =
+            document.getElementsByClassName("chart-container")[0];
+
+          const toolTipWidth = 105;
+          const toolTipHeight = 95;
+          const toolTipMargin = 15;
+
+          chart.current.subscribeCrosshairMove((param) => {
+            if (
+              param.point === undefined ||
+              !param.time ||
+              param.point.x < 0 ||
+              param.point.y < 0 ||
+              param.seriesData.get(predictionSeries.current) == undefined
+            ) {
+              setToolTip((prevState) => ({ ...prevState, display: "none" }));
+            } else {
+              const date = new Date(param.time * 1000);
+              date.setUTCMonth(date.getUTCMonth() + 1);
+              const dateStr = `${date.getUTCDate()} ${date.toLocaleString(
+                "default",
+                {
+                  month: "short",
+                }
+              )}, ${date.getFullYear()}`;
+              const data = param.seriesData.get(predictionSeries.current);
+              const price = data.value !== undefined ? data.value : data.close;
+              const content = `<div>Predicción</div>
+                <div style="font-size: 24px; margin: 4px 0px;">$${price.toFixed(
+                  3
+                )}M</div><div>${dateStr}</div>`;
+
+              const coordinate =
+                predictionSeries.current.priceToCoordinate(price);
+              let shiftedCoordinate = param.point.x - 50;
+              shiftedCoordinate = Math.max(
+                0,
+                Math.min(
+                  container.clientWidth - toolTipWidth,
+                  shiftedCoordinate
+                )
+              );
+              const coordinateY =
+                coordinate - toolTipHeight - toolTipMargin > 0
+                  ? coordinate - toolTipHeight - toolTipMargin
+                  : Math.max(
+                      0,
+                      Math.min(
+                        container.clientHeight - toolTipHeight - toolTipMargin,
+                        coordinate + toolTipMargin
+                      )
+                    );
+              setToolTip((prevState) => ({
+                ...prevState,
+                left: shiftedCoordinate + "px",
+                top: coordinateY + "px",
+                display: "block",
+                content,
+              }));
+            }
+          });
+
+          setLoading("Generar");
+
+          predictionSeries.current.update(data[data.length - 1]);
+
+          response.data.forEach((point, index) => {
+            setTimeout(() => {
+              predictionSeries.current.update({
+                time: new Date(point.year).getTime() / 1000,
+                value: parseFloat(point.pib).toFixed(3),
+              });
+              chart.current.timeScale().scrollToRealTime();
+            }, index + 1 * 40);
+          });
+        });
+
+      setLineAdded(true);
+    }
+  };
 
   return (
     <article className="chart">
@@ -116,7 +240,31 @@ export default function PibConstanteChart() {
         {data.length != 0 &&
           new Date(data[data.length - 1].time * 1000).toUTCString()}
       </p>
-      <div className="chart-container" ref={chartContainerRef}></div>
+      <div className="chart-container" ref={chartContainerRef}>
+        <div
+          style={toolTip}
+          dangerouslySetInnerHTML={{ __html: toolTip.content }}
+        />
+      </div>
+      <form className="prediction-container" onSubmit={handleButtonClick}>
+        <label>Predicción:</label>
+        <input
+          required
+          type="number"
+          name="years"
+          min="1"
+          max="5"
+          step="1"
+          value={years}
+          onChange={(e) => setYears(e.target.value)}
+        />
+        <p>años</p>
+        <button
+          style={loading ? { background: "grey", cursor: "not-allowed" } : null}
+        >
+          {loading ? loading : "Generar"}
+        </button>
+      </form>
     </article>
   );
 }
